a246a7a0056f57725f570fa2f220a47f879c1725
Abdugani <dev.abdugani@gmail.com>
2022-12-05 09:20:31 +0500

    initial

--- src/main/java/com/example/csvtask/service/DBsort.java
+++ src/main/java/com/example/csvtask/service/DBsort.java
@@ -6,6 +6,7 @@ import com.example.csvtask.repository.CitiesRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -25,11 +26,13 @@ public class DBsort implements SortService {
 
     @Override
     public List<City> sortedCitiesByCode() {
-        return repository.getAllByIdIsNotNullOrderByCityCode().stream().map(CityEntity::getCity).toList();
+        return repository.getAllByIdIsNotNullOrderByCityCode().stream().map(CityEntity::getCity)
+                .sorted(Comparator.comparing(City::getCity_code)).toList();
     }
 
     @Override
     public List<City> sortedCitiesByName() {
-        return repository.getAllByIdIsNotNullOrderByCityName().stream().map(CityEntity::getCity).toList();
+        return repository.getAllByIdIsNotNullOrderByCityName().stream().map(CityEntity::getCity)
+                .sorted(Comparator.comparing(City::getCity)).toList();
     }
 }
